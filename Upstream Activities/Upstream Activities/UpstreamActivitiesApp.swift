b4eb6820a14558b6e47a279cd3adc40c75519646
Jakob Hirschl <jakobhirschl@chello.at>
2024-05-10 07:46:33 +0200

    Initial commit with swiftlint and app icon

---
+++ Upstream Activities/Upstream Activities/UpstreamActivitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpstreamActivitiesApp.swift
+//  Upstream Activities
+//
+//  Created by Jakob Hirschl on 10.05.24.
+//
+
+import SwiftUI
+
+@main
+struct UpstreamActivitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
