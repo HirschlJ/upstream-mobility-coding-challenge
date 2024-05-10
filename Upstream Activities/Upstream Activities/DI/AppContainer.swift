c966c58a213eca77659270ef565d5594d108ee45
Jakob Hirschl <jakobhirschl@chello.at>
2024-05-10 09:34:56 +0200

    Add dependency injection with Factory

---
+++ Upstream Activities/Upstream Activities/DI/AppContainer.swift
@@ -0,0 +1,14 @@
+//
+//  AppContainer.swift
+//  Upstream Activities
+//
+//  Created by Jakob Hirschl on 10.05.24.
+//
+
+import Factory
+
+extension Container {
+    var activityApi: Factory<ActivityAPI> {
+        self { ActivityAPIImpl() }
+    }
+}
